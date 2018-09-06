3876874fbc75fb968442097670de05c25ad4906d
Unknown <chtison@users.noreply.github.com>
2018-09-07 00:54:35 +0200

    Add GoogleCloudPlatform kubernetes cluster

---
+++ google-cloud/cluster.py
@@ -0,0 +1,32 @@
+""" Create a kubernetes cluster """
+
+def GenerateConfig(context):
+	oauthScopes = [
+		'https://www.googleapis.com/auth/devstorage.read_only',
+	]
+	resources = [{
+		'name': context.env['deployment'] + '-' + context.env['name'],
+		'type': 'container.v1.cluster',
+		'properties': {
+			'zone': 'europe-west1-d',
+			'cluster': {
+				'initialClusterVersion': '1.10.6-gke.2',
+				'masterAuth': {'username': ''},
+				'nodePools': [{
+					'name': 'very-low-cost',
+					'initialNodeCount': 3,
+					'config': {
+						'machineType': 'f1-micro',
+						'preemptible': True,
+						'diskSizeGb': 10,
+						'oauthScopes': oauthScopes,
+					},
+					'management': {
+						'autoUpgrade': True,
+						'autoRepair': True,
+					},
+				}],
+			},
+		},
+	}]
+	return {'resources': resources}
